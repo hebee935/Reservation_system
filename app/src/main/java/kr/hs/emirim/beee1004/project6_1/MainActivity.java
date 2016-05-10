cfa071316467fdb3b0557bbc03a44efd1556879d
Eunbee Hong <hebee935@gmail.com>
2016-05-10 21:03:26 +0900

    #4 Third Event Programming

--- app/src/main/java/kr/hs/emirim/beee1004/project6_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/beee1004/project6_1/MainActivity.java
@@ -1,5 +1,9 @@
 package kr.hs.emirim.beee1004.project6_1;
 
+import android.annotation.TargetApi;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +13,9 @@ import android.widget.Chronometer;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import java.sql.Time;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     Chronometer chrono;
@@ -47,6 +54,42 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 timePick.setVisibility(View.VISIBLE);
                 calView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        butStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                //chrono.setTextColor(Color.GRAY);
+            }
+        });
+        butDone.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.GRAY);
+                Calendar cal = Calendar.getInstance();
+                cal.setTimeInMillis(calView.getDate());
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH)+1;
+                int date = cal.get(Calendar.DATE);
+                int hour = timePick.getHour();
+                int minute = timePick.getMinute();
+                String dateAndTime = year + "년 "+ month + "월 " + date + "일 ";
+
+                if(hour>=12) {
+                    dateAndTime += "오후 " + (hour - 12) + "시 ";
+                }else{
+                    dateAndTime += "오전 " + hour + "시 ";
+                }
+                if(minute>0){
+                    dateAndTime += minute + "분";
+                }
+
+                textResult.setText(dateAndTime);
             }
         });
     }
