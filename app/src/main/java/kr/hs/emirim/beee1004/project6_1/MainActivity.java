e4eb9270c7361ea65d259fe2043490310f874e4c
Eunbee Hong <hebee935@gmail.com>
2016-05-10 19:32:09 +0900

    #2 First Event Programming

--- app/src/main/java/kr/hs/emirim/beee1004/project6_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/beee1004/project6_1/MainActivity.java
@@ -2,12 +2,39 @@ package kr.hs.emirim.beee1004.project6_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    Chronometer chrono;
+    Button butStart,butDone;
+    RadioButton radioDate, radioTime;
+    CalendarView calView;
+    TimePicker timePick;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chrono = (Chronometer)findViewById(R.id.chronoId);
+        butStart = (Button)findViewById(R.id.startbuttonId);
+        butDone = (Button)findViewById(R.id.endbuttonId);
+        radioDate = (RadioButton)findViewById(R.id.radioIdDate);
+        radioTime = (RadioButton)findViewById(R.id.radioIdTime);
+        calView = (CalendarView)findViewById(R.id.calendarviewId);
+        timePick = (TimePicker)findViewById(R.id.timePickerId);
+        textResult = (TextView)findViewById(R.id.textViewId);
+
+        timePick.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
+
     }
+
 }
